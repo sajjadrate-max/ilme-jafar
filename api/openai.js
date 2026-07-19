f8105d81ab313447a32e1f8c720ab68c02892dfe
sajjadrate-max <sajjadrate@gmail.com>
2026-07-19 16:23:42 +0500

    Update openai.js

--- api/openai.js
+++ api/openai.js
@@ -18,7 +18,14 @@ export default async function handler(req, res) {
       })
     });
 
-    return res.status(response.status).json(data);
+    const response = await fetch(...);
+
+if (!response.ok) {
+  return res.status(response.status).json(await response.json());
+}
+
+const data = await response.json();
+return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({
       error: error.message
